fbeae8815dde7f4b763f4a3c18a99720b863eacf
Alex <alexey@nsk21.ru>
2018-04-11 12:48:11 +0700

    Added CSF/LFD reconfigure section

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # Written by Alex S Grebenschikov
 # for www.plugins-da.net
 # ============================================================
+# Version: 0.1.2 Wed Apr 11 12:40:40 +07 2018
+# Last modified: Wed Apr 11 12:40:40 +07 2018
+# ============================================================
 # Version: 0.1.1 Sat Oct  7 12:23:43 +07 2017
-# Last modified: Sat Oct  7 12:53:35 +07 2017
 # ============================================================
 
 CSF="/usr/sbin/csf";
@@ -40,6 +42,17 @@ do_install()
     chown diradmin:diradmin ${1};
 }
 
+csf_reconfig()
+{
+    echo "[OK] Disabling alerts in CSF/LFD about temporary blocks of an IP brute-forcing server";
+    perl -pi -e 's#^LF_EMAIL_ALERT = "1"#LF_EMAIL_ALERT = "0"#' /etc/csf/csf.conf;
+    echo "[OK] Disabling alerts in CSF/LFD about temporary blocks of an IP attacking Apache";
+    perl -pi -e 's#^LT_EMAIL_ALERT = "1"#LT_EMAIL_ALERT = "0"#' /etc/csf/csf.conf;
+    echo "[OK] Disabling alerts in CSF/LFD about permament blocks of an IP";
+    perl -pi -e 's#^LF_PERMBLOCK_ALERT = "1"#LF_PERMBLOCK_ALERT = "0"#' /etc/csf/csf.conf;
+
+}
+
 do_install "block_ip.sh" "http://files.plugins-da.net/dl/csf_block_ip.sh.txt";
 do_install "unblock_ip.sh" "http://files.plugins-da.net/dl/csf_unblock_ip.sh.txt";
 do_install "show_blocked_ips.sh" "http://files.plugins-da.net/dl/csf_show_blocked_ips.sh.txt";
@@ -47,6 +60,8 @@ do_install "brute_force_notice_ip.sh" "http://files.directadmin.com/services/all
 
 [ -f "/root/blocked_ips.txt" ] || touch /root/blocked_ips.txt;
 [ -f "/root/exempt_ips.txt" ] || touch /root/exempt_ips.txt;
+
+csf_reconfig;
 
 echo "[OK] Scripts installed!";
 echo "[OK] Make sure that Brute-force monitor is enabled in Directadmin with bruteforce=1";
@@ -64,4 +79,6 @@ echo "[INFO] Suggested settings:
     user_brutecount=30
 ";
 echo "You can change them in Directadmin interface at admin level or in directadmin.conf";
+
+service lfd restart;
 exit 0;
